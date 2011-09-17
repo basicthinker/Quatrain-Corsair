6182d47378bfbdf5dcddbd84e4f806e13dbd517a
basicthinker@ubuntu64 <jinglei.ren@stanzax.com>
2011-09-17 03:49:08 -0700

    Refine client log.

--- src/org/stanzax/quatrain/corsair/CorsairClient.java
+++ src/org/stanzax/quatrain/corsair/CorsairClient.java
@@ -3,12 +3,12 @@
  */
 package org.stanzax.quatrain.corsair;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.InetAddress;
 import java.util.ArrayList;
+import java.util.Collections;
 
 import org.stanzax.quatrain.client.MrClient;
 import org.stanzax.quatrain.client.ReplySet;
@@ -21,8 +21,9 @@ import org.stanzax.quatrain.hprose.HproseWrapper;
 public class CorsairClient {
 
 	public CorsairClient(InetAddress host, int port,
-			long timeout) throws IOException {
-		remote = new MrClient(host, port, new HproseWrapper(), timeout);
+			long timeout, PrintStream log) throws IOException {
+		this.remote = new MrClient(host, port, new HproseWrapper(), timeout);
+		this.log = log;
 	}
 	
 	public double evaQuery() {
@@ -34,32 +35,51 @@ public class CorsairClient {
 		}
 		groups.close();
 		
-		double totalTime = 0, caseTime, beginTime;
+		double totalTime = 0;
 		int grpCnt = 0;
 		for (Integer group : groupList) {
-			System.out.print("Group " + group + ": ");
-			caseTime = 0;
-			beginTime = System.currentTimeMillis();
-			ReplySet phones = remote.invoke(
-					String.class, "GetGroupAllUserPhone", group);
-			String phone;
-			int cnt = 0;
-			while ((phone = (String) phones.nextElement()) != null) {
-				caseTime += System.currentTimeMillis() - beginTime;
-				++cnt;
-				System.out.print(phone + " ");
-			}
-			if (cnt != 0) {
-				totalTime += caseTime / cnt;
-				++grpCnt;
-			}
-			System.out.println(": avg " + caseTime + " : total " + 
-					(System.currentTimeMillis() - beginTime));
-		}
+			totalTime += evaInvoke("GetGroupAllUserPhone", group);
+			++grpCnt;
+		} 
 		return totalTime / grpCnt;
 	}
 
+	private double evaInvoke(String method, int grpID) {
+		double avg = 0;
+		double beginTime = System.currentTimeMillis();
+		ReplySet phones = remote.invoke(
+				String.class, method, grpID);
+		String phone;
+		ArrayList<String> phoneList = new ArrayList<String>();
+		while ((phone = (String) phones.nextElement()) != null) {
+			if (phone.length() == 11) {
+				avg += System.currentTimeMillis() - beginTime;
+				phoneList.add(phone);
+			} else {
+				System.err.println("@CorsairClient.evaQuery: wrong phone " + phone);
+			}
+		}
+		phones.close();
+		
+		if (phoneList.isEmpty()) {
+			avg = System.currentTimeMillis() - beginTime;
+		} else {
+			avg /= phoneList.size();
+		}
+		
+		log.print(grpID + "\t");
+		log.print(phoneList.size() + "\t" + avg + "\t");
+		Collections.sort(phoneList); // for convenience of correctness verification
+		for (String element : phoneList) {
+			log.print("\t" + element);
+		}
+		log.println();
+		
+		return avg;
+	}
+	
 	private MrClient remote;
+	private PrintStream log;
 	
 	/**
 	 * @param args
@@ -71,13 +91,17 @@ public class CorsairClient {
 		int port = Integer.valueOf(args[1]);
 		long timeout = Long.valueOf(args[2]);
 		try {
+			PrintStream log = new PrintStream(
+					new File("corsair-client-" + args[0] + "@" + (int)System.currentTimeMillis()));
+			log.println("# GroupID\tPhoneCount\tAvgResponseTime\t\tPhoneList");
+			
 			CorsairClient client = new CorsairClient(
-					InetAddress.getByName(args[0]), port, timeout);
-			BufferedWriter log = new BufferedWriter(
-					new FileWriter(new File("corsair-client-" + args[0])));
+					InetAddress.getByName(args[0]), port, timeout, log);
+			
 			Double latency = client.evaQuery();
-			log.write(latency.toString());
 			log.close();
+			
+			System.out.println(latency.toString());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
