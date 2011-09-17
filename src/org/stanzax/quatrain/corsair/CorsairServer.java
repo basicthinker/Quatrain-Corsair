1fb1addb20e394f5e5af5b1a2ce5159fd66e9e1d
basicthinker@ubuntu64 <jinglei.ren@stanzax.com>
2011-09-17 03:36:06 -0700

    Add normal server procedure besides its multi-return version.

--- src/org/stanzax/quatrain/corsair/CorsairServer.java
+++ src/org/stanzax/quatrain/corsair/CorsairServer.java
@@ -6,11 +6,13 @@ package org.stanzax.quatrain.corsair;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Vector;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.stanzax.quatrain.client.MrClient;
 import org.stanzax.quatrain.client.ReplySet;
 import org.stanzax.quatrain.hprose.HproseWrapper;
-import org.stanzax.quatrain.io.Log;
 import org.stanzax.quatrain.io.WritableWrapper;
 import org.stanzax.quatrain.server.MrServer;
 
@@ -58,7 +60,7 @@ public class CorsairServer extends MrServer {
 	/**
 	 * For clients to get entire user phone list of target group
 	 * */	
-	public void GetGroupAllUserPhone(final int grpID) {
+	public void MrGetGroupAllUserPhone(final int grpID) {
 		preturn(db.getGroupLocalUserPhone(grpID));
 		String[] commuList = db.getGroupExternalCommu(localIP, grpID);
 		String[] commuPlusAddr;
@@ -92,6 +94,59 @@ public class CorsairServer extends MrServer {
 		}
 	}
 	
+	public void GetGroupAllUserPhone(final int grpID) {
+		final AtomicInteger endCount = new AtomicInteger();
+		final Vector<String> phones = new Vector<String>(
+				Arrays.asList(db.getGroupLocalUserPhone(grpID)));
+		final String[] commuList = db.getGroupExternalCommu(localIP, grpID);
+		String[] commuPlusAddr;
+		for (String commu : commuList) {
+			commuPlusAddr = commu.split("@");
+			final String commuID = commuPlusAddr[0];
+			final String host = commuPlusAddr[1];
+			new java.lang.Thread(new Runnable() {
+
+				@Override
+				public void run() {
+					try {
+						MrClient remote = new MrClient(
+								InetAddress.getByName(host), 
+								grpID, new HproseWrapper(), timeout);
+						ReplySet rs = remote.invoke(String.class, "GetCommuLocalUserPhone", commuID);
+						String phone;
+						while ((phone = (String) rs.nextElement()) != null) {
+							phones.add(phone);
+						}
+						rs.close();
+					} catch (IOException e) {
+						System.err.println("Calling " + host
+								+ ": " + e.getMessage());
+					} finally {
+						int current = endCount.incrementAndGet();
+						if (current == commuList.length) {
+							synchronized(CorsairServer.this) {
+								CorsairServer.this.notifyAll();
+							}
+						}
+					}
+				}
+				
+			}).start();
+		}
+		
+		synchronized(this) {
+			while (endCount.get() < commuList.length) {
+				try {
+					wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		preturn(phones.toArray());
+	}
+	
 	private DBAgent db;
 	private long timeout;
 	String localIP;
@@ -107,7 +162,6 @@ public class CorsairServer extends MrServer {
 	 * 	args[6] Timeout
 	 */
 	public static void main(String[] args) {
-		Log.setDebug(Log.ACTION);
 		int port = Integer.valueOf(args[1]);
 		int handlerCnt = Integer.valueOf(args[2]);
 		long timeout = Long.valueOf(args[6]);
