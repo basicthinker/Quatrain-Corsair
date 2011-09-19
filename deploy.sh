886146b4007839b51a7751d61788cd9976901805
basicthinker <jinglei.ren@stanzax.com>
2011-09-19 04:39:26 -0700

    Release deployment shell scripts.

--- deploy.sh
+++ deploy.sh
@@ -5,15 +5,24 @@ cd ..
 
 DB_FILE='database/corsair-db-all.sql'
 PORT=3500
+NODE_LIST=`cat node.list`
 
 if [ -f $DB_FILE ]; then
   echo ">>> Using existing $DB_FILE"
 else
   echo '<<< Dumping databases...'
+  # reset IPs in case that nodes are not ready when DB is built
+  echo 'USE corsair_smr;' > tmp.sql
+  i=1
+  for node in $NODE_LIST
+  do
+    echo "UPDATE smr_local_mgr SET ip_address='$node' WHERE id='$((10000+i))';" >> tmp.sql
+    ((++i))
+  done
+  mysql -u $DB_USER -p$DB_PASSWD < tmp.sql
+  rm tmp.sql
   mysqldump -u $DB_USER -p$DB_PASSWD --all-databases --add-drop-database > $DB_FILE
 fi
-
-NODE_LIST=`cat node.list`
 
 if [ ! -f ~/.ssh/id_dsa.pub ]; then
   ssh-keygen
@@ -55,7 +64,7 @@ echo "<<< Copying finished!"
 echo ">>> Begin DB restore..."
 for node in $NODE_LIST
 do
-  ssh root@$node "mysqladmin -u $DB_USER flush-privileges password $DB_PASSWD" 
+  ssh root@$node "mysqladmin -u $DB_USER password '$DB_PASSWD'"
   ssh root@$node "mysql -u $DB_USER -p$DB_PASSWD < ~/corsair-db-all.sql" &
 done
 wait
@@ -66,16 +75,11 @@ i=1
 for node in $NODE_LIST
 do
   scp corsair-server.jar root@$node:~/
-  ssh root@$node "nohup java -jar ~/corsair-server.jar $node $PORT 30 corsair_lmr_$i $DB_USER $DB_PASSWD 30000 > /dev/null &"
-  ssh root@$node "cat ~/server.output"
+  scp corsair-client.jar root@$node:~/
+  scp mysql-connector-java-5.1.17-bin.jar root@$node:/usr/lib/jvm/default-java/jre/lib/ext/
+  ssh root@$node "nohup java -jar ~/corsair-server.jar $node $PORT 30 corsair_lmr_$i $DB_USER $DB_PASSWD 10000 > ~/server.output 2 >& 1 &"
+  ssh root@$node "sleep 1; cat ~/server.output"
   ((++i))
 done
 echo "<<< Services started!"
 
-echo ">>> Begin starting clients..."
-for node in $NODE_LIST
-do
-  scp corsair-client.jar root@$node:~/
-  ssh root@$node "nohup java -jar ~/corsair-client.jar $node $PORT 30000 > /dev/null &"
-done
-echo "<<< All done!"
---
+++ run-eva.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+NODE_LIST=`cat node.list`
+PORT=3500
+PING_CNT=5
+LIMIT=5
+
+EVA_LOG=eva-`date +%s`.log
+
+echo ">>> Begin of ping stage"
+i=1
+for node in $NODE_LIST
+do
+  echo "Starting evaluation of ping on node-$i..."
+  ssh root@$node 'for node in '$NODE_LIST'; do ping -c 3 $node > /dev/null; ping -q -c '$PING_CNT' $node; done' > ping-node$i-`date +%s`.log &
+  ((++i))
+done
+wait
+echo "<<< End of ping stage"
+
+echo ">>> Begin of evaluation"
+i=1
+for node in $NODE_LIST
+do
+  echo "Starting evaluation client on node-$i..."
+  ssh root@$node "nohup java -jar ~/corsair-client.jar $node $PORT $LIMIT 20000" > node$i.log &
+  ((++i))
+done
+wait
+echo "<<< End of evaluation"
+
+echo -e "# NodeNum\tNormalTime\tMrTime" > $EVA_LOG
+i=1
+for node in $NODE_LIST
+do
+  value=`cat "node$i.log"`
+  echo -e "$i\t$value" >> $EVA_LOG;
+  rm node$i.log
+  ((++i))
+done
