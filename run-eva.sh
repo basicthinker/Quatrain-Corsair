78e1ee0271abd21d1518b5da0d05473aee255323
basicthinker <jinglei.ren@stanzax.com>
2011-09-24 03:35:13 -0700

    Add ulimit in deployment and run. Support request interval in corsair client.

--- run-eva.sh
+++ run-eva.sh
@@ -2,31 +2,40 @@
 LOG_DIR='log'
 NODE_LIST=`cat node.list`
 PORT=3500
-PING_CNT=5
-LIMIT=5
+PING_CNT=50
+LIMIT=50
+NOFILE=65535
 
 EVA_LOG=eva-`date +%s`.log
 
-echo ">>> Begin of ping stage"
-i=1
-for node in $NODE_LIST
-do
-  echo "Starting ping on node-$i..."
-  ssh root@$node 'for node in '$NODE_LIST'; do ping -c 3 $node > /dev/null; ping -q -c '$PING_CNT' $node; done' > $LOG_DIR/ping-node$i-`date +%s`.log &
-  ((++i))
-done
-wait
-echo "<<< End of ping stage"
+if [ ! -d $LOG_DIR ] ; then
+  mkdir -p $LOG_DIR
+fi
+
+if [ "$1" == "-p" ]; then
+  echo ">>> Begin of ping stage"
+  i=1
+  for node in $NODE_LIST
+  do
+    echo "Starting ping on node-$i..."
+    ssh root@$node 'for node in '$NODE_LIST'; do ping -c 3 $node > /dev/null; ping -q -c '$PING_CNT' $node; done' > $LOG_DIR/ping-node$i-`date +%s`.log &
+    ((++i))
+  done
+  wait
+  echo "<<< End of ping stage"
+fi
 
 echo ">>> Begin of evaluation"
 i=1
 for node in $NODE_LIST
 do
   echo "Starting evaluation client on node-$i..."
-  ssh root@$node "nohup java -jar ~/corsair-client.jar $node $PORT $LIMIT 30000" > $LOG_DIR/node$i.log &
+  ssh root@$node "ulimit -SHn $NOFILE; \
+    nohup java -jar ~/corsair-client.jar $node $PORT $LIMIT 10000 200" > $LOG_DIR/node$i.log &
   ((++i))
 done
 wait
+
 echo "<<< End of evaluation"
 
 echo -e "# NodeNum\tNormalTime\tMrTime" > $EVA_LOG
--- util/re-deploy-jar.sh
+++ util/re-deploy-jar.sh
@@ -3,25 +3,30 @@ DB_USER=root
 DB_PASSWD=tsinghua
 PORT=3500
 ROOT='..'
+NOFILE=65535
 
 NODE_LIST=`cat $ROOT/node.list`
 SERVER_LIST=(`cat $ROOT/server-ip.list`)
 
-echo ">>> Copying files..."
-for node in $NODE_LIST
-do
-  scp $ROOT/corsair-client.jar root@$node:~/ &
-  scp $ROOT/corsair-server.jar root@$node:~/ &
-done
-wait
-echo "<<< End copying..."
+if [ "$1" == "-y" ]; then
+  echo ">>> Copying files..."
+  for node in $NODE_LIST
+  do
+    scp $ROOT/corsair-client.jar root@$node:~/ &
+    scp $ROOT/corsair-server.jar root@$node:~/ &
+  done
+  wait
+  echo "<<< End copying..."
+fi
 
 i=1
 for node in $NODE_LIST
 do
-  ssh root@$node "ps aux | grep 'corsair' | grep -v 'grep' | awk '{print \$2}' | xargs kill -9 ;\
+  ssh root@$node "ps aux | grep 'corsair' | grep -v 'grep' | awk '{print \$2}' | xargs kill -9 ; \
+    ulimit -SHn $NOFILE; \
     nohup java -jar ~/corsair-server.jar ${SERVER_LIST[$((i-1))]} $PORT 100 corsair_lmr_$i $DB_USER $DB_PASSWD 20000 > ~/server.output 2>&1 & \
-    sleep 1 ; cat ~/server.output"
+    sleep 1 ; cat ~/server.output ; ulimit -n;" &
   ((++i))
 done
+wait
 
